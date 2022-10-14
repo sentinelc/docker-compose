03412172d90109998b3edd62f3c5e0a93fc8cc6f
Cedric Veilleux <veilleux.cedric@gmail.com>
2022-10-14 14:11:05 -0400

    Backup all files without creating intermediate temporary files

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,27 +2,23 @@
 
 set -e
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")"/..
+mkdir -p backups
 
 BACKUP_NAME="sentinelc-$(hostname)-$(date +"%Y%m%d_%H%M%S")"
-API_DB_BACKUP_FILE=$BACKUP_NAME-api.pgdump
-KEYCLOAK_DB_BACKUP_FILE=$BACKUP_NAME-keycloak.pgdump
+BACKUP_DEST="backups/$BACKUP_NAME.tar"
 
-echo "Creating $BACKUP_NAME.tar.gz"
+echo "Creating backup in $BACKUP_DEST"
 
-echo "- Backing up keycloak db"
-source keycloak_db.env && COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec -T keycloak_db pg_dump -Fc -n public -U $POSTGRES_USER -f /backup/$KEYCLOAK_DB_BACKUP_FILE $POSTGRES_DB
+echo "- Backing up non-postgres volumes"
+sudo tar -cpf $BACKUP_DEST --exclude=volumes/api/postgres --exclude=volumes/keycloak/postgres configs/ volumes/ credentials/
+sudo chown cveilleux:cveilleux $BACKUP_DEST
+echo "- Dumping keycloak db"
+source configs/keycloak_db.env && COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec -T keycloak_db pg_dump -Fc -n public -U $POSTGRES_USER | ./scripts/tarappend -f data/keycloak_db.pgdump -t $BACKUP_DEST
 
-echo "- Backing up api db"
-source api_db.env && COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec -T api_db pg_dump -Fc -n public -U $POSTGRES_USER -f /backup/$API_DB_BACKUP_FILE $POSTGRES_DB
-
-mkdir -p ../backups/
-echo "- Backing final backup file"
-tar -czpf ../backups/$BACKUP_NAME.tar.gz ../config.sh volumes/api_db/backup/$API_DB_BACKUP_FILE volumes/keycloak_db/backup/$KEYCLOAK_DB_BACKUP_FILE volumes/api volumes/logger_redis volumes/ssl volumes/vpnr
-outer
-
-echo "- Cleaning up temporary files"
-rm volumes/api_db/backup/$API_DB_BACKUP_FILE volumes/keycloak_db/backup/$KEYCLOAK_DB_BACKUP_FILE
+echo "- Dumping api db"
+source configs/api_db.env && COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec -T api_db pg_dump -Fc -n public -U $POSTGRES_USER  | ./scripts/tarappend -f data/api_db.pgdump -t $BACKUP_DEST
 
 echo ""
-echo "Backup complete: $(realpath ../backups/$BACKUP_NAME.tar.gz)"
+echo "Backup complete."
+
